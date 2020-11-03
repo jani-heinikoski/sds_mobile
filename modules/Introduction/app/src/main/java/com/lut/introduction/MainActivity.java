92b468d2e2bda75c50af141215c8df9cd0d23129
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2020-11-03 20:32:32 +0200

    Enabled ViewBinding and modified MainActivity.java accordingly
    - ViewBinding basically replaces the outdated function findViewByID

--- modules/Introduction/app/src/main/java/com/lut/introduction/MainActivity.java
+++ modules/Introduction/app/src/main/java/com/lut/introduction/MainActivity.java
@@ -1,14 +1,24 @@
+/*
+* Author: Jani Heinikoski
+* Created: 03.11.2020
+* Last modified: 03.11.2020
+*/
 package com.lut.introduction;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.lut.introduction.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
