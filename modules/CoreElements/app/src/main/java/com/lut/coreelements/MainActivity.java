75b09420ffe1c2e551796ba5400a326d37ffc48d
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2020-11-04 14:43:39 +0200

    Added CoreElements project, created basic GUI
    - Custom button design and animations
    - Custom actionbar
    - Support for light and dark themes

---
+++ modules/CoreElements/app/src/main/java/com/lut/coreelements/MainActivity.java
@@ -0,0 +1,19 @@
+/*
+* Author: Jani Heinikoski
+* Date created: 04.11.2020
+* Last modified: 04.11.2020
+ */
+package com.lut.coreelements;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
