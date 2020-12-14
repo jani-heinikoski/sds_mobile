bc7fc541a309d0dc697d9a05f7a5092296787fcf
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2020-12-14 14:37:45 +0200

    Created project base, learning diary update
    - Created a new project with an empty activity
    - Added proper header to MainActivity
    - Updated learning diary with today's work
    - Enabled ViewBinding

---
+++ project/MemoryLane/app/src/main/java/com/lut/memorylane/MainActivity.java
@@ -0,0 +1,20 @@
+/*
+* Author: Jani Heinikoski
+* Created: 14.12.2020
+* Last updated: see github repository https://github.com/jani-heinikoski/sds_mobile#sds_mobile
+* Sources:
+* */
+package com.lut.memorylane;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
