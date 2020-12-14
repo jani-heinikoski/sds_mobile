fe127aa5099e0383f724afcde7d9672df407a8cd
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2020-12-14 17:37:38 +0200

    Update MainActivity.java

--- project/MemoryLane/app/src/main/java/com/lut/memorylane/ui/MainActivity.java
+++ project/MemoryLane/app/src/main/java/com/lut/memorylane/ui/MainActivity.java
@@ -7,16 +7,35 @@
 package com.lut.memorylane.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import com.lut.memorylane.R;
+import com.lut.memorylane.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements IFragmentOwner {
+
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        fragmentManager = getSupportFragmentManager();
+    }
+
+    @Override
+    public void changeFragment(Fragment newFragment, boolean addToBackStack) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_activity_fragment_container, newFragment);
+        if (addToBackStack) {
+            fragmentTransaction.addToBackStack(null);
+        }
+        fragmentTransaction.commit();
     }
 }
