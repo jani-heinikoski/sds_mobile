eaa78dc2e66d976f2ae88c61bb7d724c72a6b2e1
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2020-12-14 14:42:29 +0200

    Added subpackages for source files

--- project/MemoryLane/app/src/main/java/com/lut/memorylane/ui/MainActivity.java
+++ project/MemoryLane/app/src/main/java/com/lut/memorylane/ui/MainActivity.java
@@ -4,11 +4,13 @@
 * Last updated: see github repository https://github.com/jani-heinikoski/sds_mobile#sds_mobile
 * Sources:
 * */
-package com.lut.memorylane;
+package com.lut.memorylane.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.lut.memorylane.R;
 
 public class MainActivity extends AppCompatActivity {
 
