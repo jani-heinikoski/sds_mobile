8d8ccc9667c1d1d31dc287ed8971f6249683ea2c
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2020-12-14 18:59:02 +0200

    Created empty fragment for the main menu

---
+++ project/MemoryLane/app/src/main/java/com/lut/memorylane/ui/MainMenuFragment.java
@@ -0,0 +1,31 @@
+/*
+ * Author: Jani Heinikoski
+ * Created: 14.12.2020
+ * Last updated: see github repository https://github.com/jani-heinikoski/sds_mobile#sds_mobile
+ * Sources:
+ * Android Developers, 2020. Fragment Lifecycle. [Website]. [Referred 14.12.2020]. Available: https://developer.android.com/reference/android/app/Fragment#Lifecycle
+ * */
+package com.lut.memorylane.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.lut.memorylane.databinding.FragmentMainMenuBinding;
+
+public class MainMenuFragment extends Fragment {
+    private FragmentMainMenuBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentMainMenuBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+}
