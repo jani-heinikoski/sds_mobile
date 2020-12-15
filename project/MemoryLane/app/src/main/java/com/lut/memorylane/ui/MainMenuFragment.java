e761532b926c6cb85807beed46f18fd7d7dd97e5
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2020-12-15 14:44:26 +0200

    Functional navigation for the main menu

--- project/MemoryLane/app/src/main/java/com/lut/memorylane/ui/MainMenuFragment.java
+++ project/MemoryLane/app/src/main/java/com/lut/memorylane/ui/MainMenuFragment.java
@@ -7,6 +7,7 @@
  * */
 package com.lut.memorylane.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,12 +21,49 @@ import com.lut.memorylane.databinding.FragmentMainMenuBinding;
 
 public class MainMenuFragment extends Fragment {
     private FragmentMainMenuBinding binding;
+    private IFragmentOwner fragmentOwner;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof IFragmentOwner) {
+            fragmentOwner = (IFragmentOwner) context;
+        } else {
+            System.err.println("OwnerActivity must implement IFragmentOwner interface.");
+            System.exit(-1);
+        }
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentMainMenuBinding.inflate(inflater, container, false);
+        initializeMainMenuButtons();
         return binding.getRoot();
+    }
+
+    private void initializeMainMenuButtons() {
+        // Switch to Speed Test fragment when corresponding button is clicked
+        binding.fragmentMainButtonSpeedTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentOwner.changeFragment(new SpeedTestFragment(), true);
+            }
+        });
+        // Switch to Memory Test fragment when corresponding button is clicked
+        binding.fragmentMainButtonMemoryTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentOwner.changeFragment(new MemoryTestFragment(), true);
+            }
+        });
+        // Switch to High Score fragment when corresponding button is clicked
+        binding.fragmentMainButtonHighScores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentOwner.changeFragment(new HighScoreFragment(), true);
+            }
+        });
     }
 
 }
