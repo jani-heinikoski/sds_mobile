9e927dcd9494d4728e5dbc6edd6a5452f5144bb2
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2020-12-17 12:21:00 +0200

    Updated interface to work with navigation

--- project/MemoryLane/app/src/main/java/com/lut/memorylane/ui/IFragmentOwner.java
+++ project/MemoryLane/app/src/main/java/com/lut/memorylane/ui/IFragmentOwner.java
@@ -4,8 +4,14 @@
  */
 package com.lut.memorylane.ui;
 
+import android.view.View;
+
+import androidx.annotation.DrawableRes;
+import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
 
 public interface IFragmentOwner {
     void changeFragment(Fragment newFragment, boolean addToBackStack);
+    void changeActionbar(@DrawableRes int iconDrawableID, @StringRes int titleStringResID, View.OnClickListener onClickListener);
+    void popBackStack();
 }
