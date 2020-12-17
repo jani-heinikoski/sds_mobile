534f11bda37c46afd907ec111dee735bba9b6044
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2020-12-17 12:21:29 +0200

    Added implementations for new IFragmentOwner interface

--- project/MemoryLane/app/src/main/java/com/lut/memorylane/ui/MainActivity.java
+++ project/MemoryLane/app/src/main/java/com/lut/memorylane/ui/MainActivity.java
@@ -6,12 +6,18 @@
 * */
 package com.lut.memorylane.ui;
 
+import androidx.annotation.DrawableRes;
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 
 import com.lut.memorylane.R;
 import com.lut.memorylane.databinding.ActivityMainBinding;
@@ -19,11 +25,12 @@ import com.lut.memorylane.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity implements IFragmentOwner {
 
     private FragmentManager fragmentManager;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         fragmentManager = getSupportFragmentManager();
@@ -38,5 +45,30 @@ public class MainActivity extends AppCompatActivity implements IFragmentOwner {
             fragmentTransaction.addToBackStack(null);
         }
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public void changeActionbar(@DrawableRes int iconDrawableID, @StringRes int titleStringResID, View.OnClickListener onClickListener) {
+        Drawable drawable = ContextCompat.getDrawable(this, iconDrawableID);
+        String title = getString(titleStringResID);
+
+        if (drawable != null) {
+            binding.mainActivityActionBarButtonHomeIcon.setBackground(drawable);
+        } else {
+            System.err.println("Failed to retrieve drawable resource, id was " + iconDrawableID);
+        }
+
+        if (title != null) {
+            binding.mainActivityActionBarTextviewTitle.setText(title);
+        } else {
+            System.err.println("Failed to retrieve string resource, id was " + titleStringResID);
+        }
+
+        binding.mainActivityActionBarButtonHomeIcon.setOnClickListener(onClickListener);
+    }
+
+    @Override
+    public void popBackStack() {
+        fragmentManager.popBackStack();
     }
 }
