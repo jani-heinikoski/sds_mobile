10ca2b89b4bf94c5479b574a90a1c73d2856c3ba
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2020-12-17 19:00:24 +0200

    Removed unnecessary imports

--- project/MemoryLane/app/src/main/java/com/lut/memorylane/ui/SpeedTestFragment.java
+++ project/MemoryLane/app/src/main/java/com/lut/memorylane/ui/SpeedTestFragment.java
@@ -8,25 +8,17 @@
 package com.lut.memorylane.ui;
 
 import android.animation.ObjectAnimator;
-import android.animation.ValueAnimator;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
-import android.view.animation.ScaleAnimation;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.lut.memorylane.databinding.FragmentSpeedTestBinding;
-import com.lut.memorylane.utility.SharedViewModel;
 
 public class SpeedTestFragment extends Fragment {
 
