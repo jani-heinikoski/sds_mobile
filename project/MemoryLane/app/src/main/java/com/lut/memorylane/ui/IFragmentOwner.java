d581f43c43197a9dc9e927f4f78a52d777f4d987
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2020-12-17 18:16:28 +0200

    Deprecated changeActionbar method

--- project/MemoryLane/app/src/main/java/com/lut/memorylane/ui/IFragmentOwner.java
+++ project/MemoryLane/app/src/main/java/com/lut/memorylane/ui/IFragmentOwner.java
@@ -4,14 +4,9 @@
  */
 package com.lut.memorylane.ui;
 
-import android.view.View;
-
-import androidx.annotation.DrawableRes;
-import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
 
 public interface IFragmentOwner {
     void changeFragment(Fragment newFragment, boolean addToBackStack);
-    void changeActionbar(@DrawableRes int iconDrawableID, @StringRes int titleStringResID, View.OnClickListener onClickListener);
     void popBackStack();
 }
