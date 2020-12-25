53b2c15ff34c6fafda6aa599b7d5ce694583bcf4
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2020-12-25 21:26:07 +0200

    Updated Highscore class to match Room entity class
    - Added @Entity annotation (changed database's table name to high_score)
    - Enabled primarykey auto generate function (generates id's by itself)
    - Changed nickname to timestamp due to time limitations

--- project/MemoryLane/app/src/main/java/com/lut/memorylane/data/HighScore.java
+++ project/MemoryLane/app/src/main/java/com/lut/memorylane/data/HighScore.java
@@ -1,12 +1,30 @@
+/*
+* Author: Jani Heinikoski
+* Created: 25.12.2020
+* Last updated: see github repository https://github.com/jani-heinikoski/sds_mobile#sds_mobile
+* Sources:
+* Android Developers, 2020. Defining data using Room entities. [Website]. [Referred 25.12.2020]. Available: https://developer.android.com/training/data-storage/room/defining-data
+ */
 package com.lut.memorylane.data;
 
-public class HighScore {
-    private int score;
-    private String name;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
-    public HighScore(int score, String name) {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+@Entity(tableName = "high_score")
+public class HighScore {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private int score;
+    private String timestamp;
+
+    public HighScore(int score) {
         this.score = score;
-        this.name = name;
+        this.timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.US).format(new Date());;
     }
 
     public int getScore() {
@@ -17,11 +35,19 @@ public class HighScore {
         this.score = score;
     }
 
-    public String getName() {
-        return name;
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
