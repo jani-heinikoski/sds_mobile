3eb796ecd1d6af9933ddfdfa9a297d5af59116de
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2020-12-25 16:59:16 +0200

    Data holder class for the high score objects

---
+++ project/MemoryLane/app/src/main/java/com/lut/memorylane/data/HighScore.java
@@ -0,0 +1,27 @@
+package com.lut.memorylane.data;
+
+public class HighScore {
+    private int score;
+    private String name;
+
+    public HighScore(int score, String name) {
+        this.score = score;
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
